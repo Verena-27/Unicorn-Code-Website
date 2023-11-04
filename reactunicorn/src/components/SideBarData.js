fa70dc064895feda8b7c1acf7d857fcd6c8e8f39
Weintaube <john.lena@t-online.de>
2023-11-04 13:54:10 +0100

    sidebar

---
+++ reactunicorn/src/components/SideBarData.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+export const SideBarData = [
+    {
+        title: "Willkommen",
+        link: "/home"
+    },
+    {
+        title: "Termine",
+        link: "/dates"
+    },
+    {
+        title: "Anmeldung",
+        link: "/register"
+    },
+    {
+        title: "Über uns",
+        link: "/about us"
+    },
+    {
+        title: "Dateien",
+        link: "/files"
+    },
+    {
+        title: "Kontakt",
+        link: "/contact"
+    }
+]
--- reactunicorn/src/components/Sidebar.jsx
+++ reactunicorn/src/components/Sidebar.jsx
@@ -1,10 +1,26 @@
 
 import '../App.css';
 import React from "react";
+import { SideBarData } from './SideBarData';
 
 function Sidebar(){
     return(
-        <div className="Sidebar"></div>
+        <div className="Sidebar">
+            <ul className="SidebarList">
+            {SideBarData.map((val, key)=>{
+                    return(
+                        <li 
+                        key={key} 
+                        className="row" 
+                        id={window.location.pathname == val.link ? "active": ""}
+                        onClick={() => {window.location.pathname = val.link}}>
+                            <div>{val.title}</div>
+                        </li>
+                    )
+                })}
+             </ul>
+        
+        </div>
     )
 }
 
