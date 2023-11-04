78207e94a3a5f7ef2d2f540691e22e9559cb8097
Weintaube <john.lena@t-online.de>
2023-11-04 11:16:22 +0100

    new project

---
+++ reactunicorn/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className= "App">
+      <Sidebar></Sidebar>
+    </div>
+  );
+}
+
+export default App;
