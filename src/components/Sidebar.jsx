0c1c24ce2049a7c72dac3b04f08a88d8487620b6
Verena <verena.bolling@stud.uni-hannover.de>
2024-11-23 14:53:14 +0100

    sidebar reactive update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,26 +3,41 @@ import '../App.css';
 import React from "react";
 import { NavLink } from 'react-router-dom';
 import { SideBarData } from './SideBarData';
+import { useState } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //id={window.location.pathname == val.link ? "active": ""}
 //onClick={() => {window.location.pathname = val.link}}
 function Sidebar(){
+    const [isOpen, setIsOpen] = useState(false);
+    const togglesidebar = () => {
+        setIsOpen(!isOpen);
+    }
     return(
-        <div className="Sidebar">
-            <ul className="SidebarList">
-            {SideBarData.map((val, key)=>{
-                    return(
-                        <li 
-                        key={key} 
-                        className="row" >
-                            <NavLink to={val.link}>{val.title}</NavLink>
-                        </li>
-                    )
-                })}
-             </ul>
-        
+        <div>
+        {/* hamburger menu */}
+        <button className={`hamburger ${isOpen ? "open" : ""}`} onClick={togglesidebar}>
+        ☰
+        </button>
+            {/* sidebar */}
+            <div className="Sidebar">
+                <ul className="SidebarList">
+                {SideBarData.map((val, key)=>{
+                        return(
+                            <li 
+                            key={key} 
+                            className="row" >
+                                <NavLink to={val.link}>{val.title}</NavLink>
+                            </li>
+                        )
+                    })}
+                </ul>
+            
+            </div>
+            {/* Overlay */}
+            <div className={`overlay ${isOpen ? "visible" : ""}`} onClick={togglesidebar}>
+            </div>
         </div>
     )
 }
